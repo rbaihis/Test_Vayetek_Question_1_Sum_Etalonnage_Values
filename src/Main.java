a811a9b6f8ce9d8cb607289a8bfb4d5781a65fb2
saif rabbeh <rabbehs>
2025-01-10 01:36:27 +0100

    adding solution

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+
+    private static int getEtalonnageValue(String line, StringBuilder digitsHolder){
+
+        digitsHolder.setLength(0);
+
+        for (int i=0 ; i<line.length(); i++)
+            if(Character.isDigit(line.charAt(i)))
+                digitsHolder.append(line.charAt(i));
+
+        if(digitsHolder.isEmpty())
+            return 0;
+
+        int firstDigit =  Character.getNumericValue(digitsHolder.charAt(0));
+        int lastDigit =  Character.getNumericValue(digitsHolder.charAt(digitsHolder.length()-1));
+
+        return firstDigit*10 + lastDigit;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        String filePath = "resources/document.txt";
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+
+            StringBuilder reusableDigitHolder = new StringBuilder();
+            int sum = br.lines()
+                    .mapToInt( line-> getEtalonnageValue(line,reusableDigitHolder))
+                    .sum();
+
+            System.out.println("\n\nSum : "+sum);
+
+        }catch (IOException e){
+            log.warning("Error occurred when manipulating file : "+e.getMessage());
+            throw new IOException(e.getCause());
+        }
+    }
+
+}
